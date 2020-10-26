d3a634cdadc36a888c7783f3875d34a81a5c02f4
wisnubayuaji <wisnubayu34@gmail.com>
2020-10-26 10:09:29 +0700

    benerin folder

--- src/App.java
+++ src/App.java
@@ -16,9 +16,9 @@ public class App {
         System.out.printf("%%-10d : %-10d%n", i);
         double f = 5675482.982;
         System.out.println("f : " + f);
-        System.out.printf("%%f : %f%n" + f);
-        System.out.printf("%%.2f : %.2f%n" + f);
-        System.out.printf("%%12.2f : %12.2f%n" + f);
-        System.out.printf("%%,12.2f : %,12.2f%n" + f);
+        System.out.printf("%%f : %f%n", + f);
+        System.out.printf("%%.2f : %.2f%n", + f);
+        System.out.printf("%%12.2f : %12.2f%n", + f);
+        System.out.printf("%%,12.2f : %,12.2f%n", + f);
     }
 }
